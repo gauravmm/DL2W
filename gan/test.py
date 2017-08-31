12bd98c44033b5e6eaa2f37642bc7ec0f0f6ce4e
Houssam Zenati <houssam.zenati@student.ecp.fr>
2017-08-31 11:02:26 +0800

    Update test.py
    I was trying to run the code and it couldn't run... Then I thought "what's the point of testing a gan? is it to see it's accuracy? no, it's not a CNN, it must only be about downloading the trained the model (tf.train.Supervisor(logdir="gan/train_logs/ .....)) and see what images could be generated with the trained generator, so I should maybe save the images in another file to see them (the logwriter in gan/test_logs/). 
    Now it works and it also displays the images with the step being displayed, same as the modification in train.py file.
    Thanks for this wonderful code!

--- gan/test.py
+++ gan/test.py
@@ -4,13 +4,13 @@
 import tensorflow as tf
 from data import cifar10, utilities
 from . import dcgan
+import logging
 
-# Create CIFAR-10 input
+logger = logging.getLogger("gan.test")
+
 BATCH_SIZE = 256
-data_generator = map((lambda image, label: (image*2. - 1., label)), utilities.infinite_generator(cifar10.get_train(), BATCH_SIZE))
 
 # Sample noise from random normal distribution
-n_input = tf.placeholder(tf.float32, shape=cifar10.get_shape_input(), name="input")
 
 random_z = tf.random_normal([BATCH_SIZE, 100], mean=0.0, stddev=1.0, name='random_z')
 
@@ -19,17 +19,17 @@ generator = dcgan.generator(random_z, is_training=True, name='generator')
 
 # Add summaries to visualise output images and losses
 generator_visualisation = tf.cast(((generator / 2.0) + 0.5) * 255.0, tf.uint8)
-tf.summary.image('summary/generator', generator_visualisation, max_outputs=8)
+summary_generator = tf.summary. \
+                    image('summary/generator', generator_visualisation, max_outputs=8)
 
 sv = tf.train.Supervisor(logdir="gan/train_logs/", save_summaries_secs=None, save_model_secs=None)
 
 batch = 0
 with sv.managed_session() as sess:
+    logwriter = tf.summary.FileWriter("gan/test_logs/", sess.graph)
     while not sv.should_stop():
         if batch > 0 and batch % 100 == 0:
-            logger.debug('Step {} of {}.'.format(batch, NUM_BATCHES))
-
-        inp, _ = next(data_generator)
-        sess.run(generator_visualisation)
-        
+            logger.debug('Step {}.'.format(batch))
+        sum_gen = sess.run(summary_generator)
+        logwriter.add_summary(sum_gen, batch)
         batch += 1
